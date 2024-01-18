893480ea3b2b978f299bba32ba78a08342560ab1
Alireza-mir <157066251+Alireza-mir@users.noreply.github.com>
2024-01-19 00:34:39 +0330

    Binary-Tree
    Binary-Tree

--- Program.cs
+++ Program.cs
@@ -4,266 +4,211 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prefix_infix_postfix
+namespace Binary_tree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("prefix--to--infix--press 1");
-
-            Console.WriteLine("infix--to--postfix--press 2");
-
-            Console.WriteLine("prefix--to--postfix--press 3");
-
-            Console.WriteLine("infix--to--prefix--press 4");
-
-            Console.WriteLine("postfix--to--infix--press 5");
-
-            Console.WriteLine("postfix--to--prefix--press 6");
-
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            switch (n)
-            {
-                case 1:
-                    {
-                        Console.WriteLine("PREFIX :");
-                        Console.WriteLine(PrefixToInfix.ConvertPrefixToInfix(Console.ReadLine()));
-                    }
-                    break;
-                case 2:
-                    {
-                        Console.WriteLine("INFIX :");
-                        Console.WriteLine(InfixToPostfix.ConvertToPostfix(Console.ReadLine()));
-                    }
-                    break;
-                case 3:
-                    {
-                        Console.WriteLine("PREFIX :");
-                        string q = (InfixToPostfix.ConvertToPostfix(Console.ReadLine()));
-                        Console.WriteLine(InfixToPostfix.ConvertToPostfix(q));
-                    }
-                    break;
-                case 4:
-                    {
-                        Console.WriteLine("INFIX :");
-                        Console.WriteLine(infixtoprefix.converttoinfix(Console.ReadLine()));
-                    }
-                    break;
-                case 5:
-                    {
-                        Console.WriteLine("POSTFIX :");
-                        Console.WriteLine(postfixtoinfix.convertpostfixtoinfix(Console.ReadLine()));
-
-                    }
-                    break;
-                case 6:
-                    {
-                        Console.WriteLine("POSTFIX :");
-
-                        string a = postfixtoinfix.convertpostfixtoinfix(Console.ReadLine());
-                        string aa = a.Replace(")", "(");
-
-                        string b = infixtoprefix.converttoinfix(aa);
-                        string bb = b.Replace("(", "").Replace(")", "");
-                        Console.WriteLine(bb);
-
-                    }
-                    break;
+            BinarytreeNode A = new BinarytreeNode(18);
+            BinarytreeNode B = new BinarytreeNode(16);
+            BinarytreeNode C = new BinarytreeNode(4);
+            BinarytreeNode D = new BinarytreeNode(6, C, null);
+            BinarytreeNode E = new BinarytreeNode(17, B, A);
+            BinarytreeNode F = new BinarytreeNode(15, D, E);
 
 
-            }
-
-
-
-
+            //BinarytreeNode.ADDNode(F,1);
+            //BinarytreeNode.DeleteNode(F, 17);
+            Console.Write("tree is :");
+            BinarytreeNode.printtree(F);
+            Console.WriteLine("");
+            Console.WriteLine("number of levels =" + BinarytreeNode.numberoflevels(F));
+            Console.WriteLine("Sum of tree node =" + BinarytreeNode.SumofNode(F));
+            Console.WriteLine("number of nodes in tree =" + BinarytreeNode.countNode(F));
+            Console.WriteLine("number of leaves in the tree is :" + BinarytreeNode.countLeaves(F));
+            Console.Write("preorder :");
+            BinarytreeNode.preorder(F);
+            Console.Write("    inorder :");
+            BinarytreeNode.inorder(F);
+            Console.Write("     postorder :");
+            BinarytreeNode.postorder(F);
 
             Console.ReadKey();
 
 
 
 
+
+
         }
     }
-    class Stackm<T>
+    class BinarytreeNode
     {
-        private static int top;
-        private T[] items;
+        public int data;
+        public BinarytreeNode Left;
+        public BinarytreeNode Right;
 
-
-        public Stackm(int n)
+        public BinarytreeNode(int D)
         {
-            items = new T[n];
-
-            top = -1;
-
+            data = D;
+            Left = null;
+            Right = null;
         }
 
-        public void add(T item)
+        public BinarytreeNode(int D, BinarytreeNode L, BinarytreeNode R)
         {
-            if (isfull())
-            {
-                Console.WriteLine("stack is full");
+            data = D;
+            Left = L;
+            Right = R;
+        }
 
+        public static BinarytreeNode ADDNode(BinarytreeNode R, int Dataa)
+        {
+            if (R == null)
+            {
+                return new BinarytreeNode(Dataa);
             }
+            if (Dataa < R.data)
+            {
+                R.Left = ADDNode(R.Left, Dataa);
+            }
+            else if (Dataa > R.data)
+            {
+                R.Right = ADDNode(R.Right, Dataa);
+            }
+
+            return R;
+        }
+
+        public static BinarytreeNode minnode(BinarytreeNode R)
+        {
+            BinarytreeNode min = R;
+
+            while (min.Left != null)
+            {
+                min = min.Left;
+            }
+            return min;
+        }
+        public static BinarytreeNode DeleteNode(BinarytreeNode R, int key)
+        {
+            if (R == null)
+                return null;
+            if (key < R.data)
+                R.Left = DeleteNode(R.Left, key);
+            else if (key > R.data)
+                R.Right = DeleteNode(R.Right, key);
             else
-                items[++top] = item;
-        }
-        public T delete()
-        {
-            if (top == -1)
             {
-                Console.WriteLine("stack is empty");
-            }
-            return items[top--];
-
-        }
-
-        public Boolean isfull()
-        {
-            if (top == items.Length - 1)
-            {
-                return true;
-            }
-            else return false;
-        }
-        public T peek()
-        {
-            if (top == -1)
-            {
-                Console.WriteLine("the stack is empty");
-            }
-            return items[top];
-        }
-        public int count()
-        {
-            return top + 1;
-        }
-
-    }
-    class postfixtoinfix
-    {
-
-        public static string convertpostfixtoinfix(string postfix)
-        {
-            Stackm<string> st = new Stackm<string>(50);
-
-            foreach (var item in postfix)
-            {
-                char h = item;
-                if (h == '+' || h == '-' || h == '*' || h == '/')
+                if (R.Left == null)
                 {
-                    string operand1 = st.delete();
-                    string operand2 = st.delete();
-                    string newexpression = "(" + operand2 + h + operand1 + ")";
-                    st.add(newexpression);
-
+                    BinarytreeNode temp = R.Right;
+                    R = null;
+                    return temp;
                 }
-                else
+                else if (R.Right == null)
                 {
-                    st.add(h.ToString());
+                    BinarytreeNode temp = R.Left;
+                    R = null;
+                    return temp;
                 }
 
+                BinarytreeNode tempe = BinarytreeNode.minnode(R.Right);
+                R.data = tempe.data;
+                R.Right = DeleteNode(R.Right, tempe.data);
 
             }
+            return R;
 
-            return st.delete();
         }
 
-    }
-    public class PrefixToInfix
-    {
-        public static string ConvertPrefixToInfix(string prefix)
-        {
-            Stack<string> st = new Stack<string>();
 
-            for (int i = prefix.Length - 1; i >= 0; i--)
+
+        public static int numberoflevels(BinarytreeNode R)
+        {
+            if (R == null)
             {
-                char c = prefix[i];
-
-                if (c == '+' || c == '-' || c == '*' || c == '/')
-                {
-                    string operand1 = st.Pop();
-                    string operand2 = st.Pop();
-                    string infixExpression = "(" + operand1 + c + operand2 + ")";
-                    st.Push(infixExpression);
-                }
-                else
-                {
-                    st.Push(c.ToString());
-                }
-            }
-
-            return st.Pop();
-        }
-
-    }
-
-    class InfixToPostfix
-    {
-        public static int Precedence(char c)
-        {
-            if (c == '+' || c == '-')
-                return 1;
-            else if (c == '*' || c == '/')
-                return 2;
-            else
                 return 0;
-        }
 
-        public static string ConvertToPostfix(string infix)
-        {
-            string postfix = "";
-            Stack<char> stack = new Stack<char>();
-
-            for (int i = 0; i < infix.Length; i++)
-            {
-                char c = infix[i];
-                if (Char.IsLetterOrDigit(c))
-                {
-                    postfix += c;
-                }
-                else if (c == '(')
-                {
-                    stack.Push(c);
-                }
-                else if (c == ')')
-                {
-                    while (stack.Count > 0 && stack.Peek() != '(')
-                    {
-                        postfix += stack.Pop();
-                    }
-                    stack.Pop();
-                }
-                else
-                {
-                    while (stack.Count > 0 && Precedence(c) <= Precedence(stack.Peek()))
-                    {
-                        postfix += stack.Pop();
-                    }
-                    stack.Push(c);
-                }
             }
 
-            while (stack.Count > 0)
-            {
-                postfix += stack.Pop();
-            }
-
-            return postfix;
+            int leftH = numberoflevels(R.Left);
+            int rightH = numberoflevels(R.Right);
+            return 1 + Math.Max(leftH, rightH);
         }
-    }
-    class infixtoprefix
-    {
-        public static string converttoinfix(string infix)
+
+        public static int countNode(BinarytreeNode r)
         {
-            string reverseinfix = new string(infix.Reverse().ToArray());
-            string postfix = InfixToPostfix.ConvertToPostfix(reverseinfix);
-            string prefix = new string(postfix.Reverse().ToArray());
-            return prefix;
+            if (r == null)
+            {
+                return (0);
+
+            }
+            else return (1 + countNode(r.Left) + countNode(r.Right));
 
         }
+        public static int countLeaves(BinarytreeNode R)
+        {
+            if (R == null)
+            {
+                return 0;
+            }
+            if (R.Left == null && R.Right == null)
+            {
+                return 1;
+            }
+            return countLeaves(R.Left) + countLeaves(R.Right);
+
+        }
+
+        public static void preorder(BinarytreeNode R)
+        {
+            if (R == null)
+                return;
+            Console.Write(R.data + ",");
+            preorder(R.Left);
+            preorder(R.Right);
+        }
+        public static void inorder(BinarytreeNode R)
+        {
+            if (R == null)
+                return;
+            inorder(R.Left);
+            Console.Write(R.data + ",");
+            inorder(R.Right);
+        }
+        public static void postorder(BinarytreeNode R)
+        {
+            if (R == null)
+                return;
+            postorder(R.Left);
+            postorder(R.Right);
+            Console.Write(R.data + ",");
+
+        }
+        public static int SumofNode(BinarytreeNode R)
+        {
+            if (R == null) return 0;
+            return SumofNode(R.Left) + SumofNode(R.Right) + R.data;
+
+        }
+        public static void printtree(BinarytreeNode R)
+        {
+
+
+            if (R != null)
+            {
+                printtree(R.Left);
+                Console.Write(R.data + " ");
+                printtree(R.Right);
+
+            }
+        }
+
+
+
+
 
 
 
